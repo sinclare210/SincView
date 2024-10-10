482fb83539707ec9e1533acdca256f9d34f7e70d
Sinclair <sinclairolajuwon@gmail.com>
2024-10-10 06:58:52 -0500

    starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import Navbar from './components/Navbar'
+import Feed from './components/Feed'
+import VideoDetail from './components/VideoDetail'
+import ChannelDetail from './components/ChannelDetail'
+import SearchFeed from './components/SearchFeed'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+        <Box sx={{backgroundColor : "#000"}}>
+            <Navbar />
+            <Routes>
+              <Route path='/' element={<Feed/>}/>
+              <Route path= "/video/:id" element={<VideoDetail/>}/>
+              <Route path= "/channel/:id" element={<ChannelDetail/>}/>
+              <Route path= "/search/:searchTerm" element={<SearchFeed/>}/>
+            </Routes>
+        </Box>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Feed.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+const Feed = () => {
+  return (
+    <Stack sx={{flexDirection:{sx:"column", md:"row"}}}>
+      <Box sx={{height: {sx: "auto", md: "92vh"}, borderRight:"1px solid #3d3d3d", px:{sx:0, md:2}}}>
+        Sidebar
+        <Typography
+      </Box>
+    </Stack>
+  )
+}
+
+export default Feed
