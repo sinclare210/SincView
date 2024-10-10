6fe54094bb0445edf41f0904d265314eb6dbd93e
Sinclair <sinclairolajuwon@gmail.com>
2024-10-10 16:27:07 -0500

    worked on videoCard

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Box,CardContent,CardMedia, Typography } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
-import { demoThumbnailUrl,demoVideoUrl,demoVideoTitle,demoChannelUrl,demoChannelTitle } from './utils/constants'
+import { demoProfilePicture } from './utils/constants'
 
 
-const ChannelCard = () => {
+const ChannelCard = ({channelDetail}) => {
   return (
-    <div>ChannelCard</div>
+    <Box
+    sx={{
+        boxShadow:"none",
+        borderRadius:"20px"
+    }}
+    >
+
+        <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent sx={{display:"flex", flexDirection:"column", justifyContent:"center", textAlign:"center", color:"#FFF"}}>
+            <CardMedia
+                image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+                alt={channelDetail?.snippet?.title}
+                sx={{borderRadius:"50%", height:"180px", width:"180px", mb: 2, border:"1px solid #e3e3e3"}}
+            />
+            <Typography variant='h6'>
+                {channelDetail?.snippet?.title}
+                <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
+            </Typography>
+        </CardContent>
+        </Link>
+    </Box>
   )
 }
 
