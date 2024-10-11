fb861aea8b88d5c1569486c235fa9eeba8238aa1
Sinclair <sinclairolajuwon@gmail.com>
2024-10-10 19:43:30 -0500

    video detail next

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import Sidebar from './Sidebar'
+import Videos from './Videos'
+import { useParams } from 'react-router-dom'
+
+import { fetchFromApi } from './utils/FetchFromApi'
 
 const SearchFeed = () => {
+
+  const [videos, setVideos] = useState([]);
+  const {searchTerm} = useParams();
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`)
+.then((data) => {
+  setVideos(data.items)
+})  },[searchTerm])
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{
+        overflowY:"auto", height:"90vh", flex:"2"
+      }}>
+          <Typography variant='h4' fontWeight="bold" mb={2} sx={{
+        color: "white"
+      }}>
+        Search result for:<span style={{color: "#FC1503"}}> {searchTerm} Videos</span>
+      </Typography>
+      <Videos videos={videos}/>
+      </Box>
+    
   )
 }
 
